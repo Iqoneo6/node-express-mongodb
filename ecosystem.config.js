e492c6fdda954588ef61f0a0bf82aad9feff8b58
Iqoneo6 <142778317+Iqoneo6@users.noreply.github.com>
2024-09-14 14:29:12 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,23 +3,4 @@ module.exports = {
     name: "sample",
     script: 'server.js',
     watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
-  }],
-
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
-  }
-};
-this file is ecosystem.conf.js file
-That can be the first tool for some open tool
+  }]
