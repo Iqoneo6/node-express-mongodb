208297d45be00200d33a4e1b47e070e91b75588f
Iqoneo6 <sachinvel59@gmail.com>
2024-10-10 12:39:00 +0530

    ciandcd

--- ecosystem.config copy.js
+++ ecosystem.config copy.js
@@ -1,7 +1,19 @@
 module.exports = {
-  apps : [{
-    name: 'sample',
-    script: 'server.js',
-    watch: '.'
-  }]
+  apps: [
+    {
+      name: 'my-web-app',
+      script: './server.js',
+      instances: 'max',
+      exec_mode: 'cluster',
+      watch: false,
+      env: {
+        NODE_ENV: 'development',
+        PORT: 3000,
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 8000,
+      },
+    },
+  ],
 };
